497d44392723a69a448c0511c7c91cad6da0da51
LemurPwned <jakub.mojsiejuk@gmail.com>
2018-07-11 13:30:36 +0100

    fixed hostanme resolution

--- pyspark_app/scripts/publish_emb.sh
+++ pyspark_app/scripts/publish_emb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-for file in ./EMB3_dataset_2017 /*
+for file in /scripts/EMB3_dataset_2017/*.csv;
 do
   echo "New $file to stream"
   python producer_kafka.py $file 'emb' 'emb3'
