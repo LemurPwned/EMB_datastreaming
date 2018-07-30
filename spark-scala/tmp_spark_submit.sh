1eb2202e06ce588bbd9a896043071baee5c8bbb2
Jakub Mojsiejuk <jakub.mojsiejuk@gmail.com>
2018-07-30 15:54:55 +0200

    updated job submit script

--- spark-scala/tmp_spark_submit.sh
+++ spark-scala/tmp_spark_submit.sh
@@ -1,1 +1,1 @@
-/opt/spark/bin/spark-submit --class cassandra_job.CassandraInteg --master local --jars kafka-clients-1.1.1.jar,commons-math3-3.6.1.jar,stl-decomp-4j-1.0.4-SNAPSHOT.jar,datastax_cassandra_connector.jar target/scala-2.11/cassandrainteg_2.11-1.0.jar
+/opt/spark/bin/spark-submit --class cassandra_job.CassandraInteg --master local --jars avro-1.8.2.jar,kafka-clients-1.1.1.jar,commons-math3-3.6.1.jar,stl-decomp-4j-1.0.4-SNAPSHOT.jar,datastax_cassandra_connector.jar target/scala-2.11/cassandrainteg_2.11-1.0.jar
