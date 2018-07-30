1bb4c4ffe994863560e56eb03adc5108d9f69537
Jakub Mojsiejuk <jakub.mojsiejuk@gmail.com>
2018-07-30 15:50:24 +0200

    producer that complies to avro schemas

--- kafka-cassandra-integration/kafka_producer.py
+++ kafka-cassandra-integration/kafka_producer.py
@@ -16,6 +16,22 @@ def emb_line_processing(line):
     flag += line
     return flag
 
+non_binary_vals = ['(na)', '0.00', '0']
+def msg_processing(msg):
+    msg[-1] = msg[-1].replace('\r\n', '')
+    correct_vals = []
+    flag = 'N'
+    for value in msg[2:]:
+        if value in non_binary_vals:
+            flag = 'A'
+            correct_vals.append(0.00)
+        else:
+            correct_vals.append(float(value))
+    assert len(correct_vals) == len(msg[2:])
+    return {"flag": flag, "timestamp": msg[0] + " " + msg[1] + 'Z',
+            "v1": correct_vals[0], "v2": correct_vals[1], "v3": correct_vals[2], 
+            "v4": correct_vals[3], "v5": correct_vals[4]}
+
 schema = avro.schema.Parse(open("emb_schema.avsc").read())
 spark_schema = avro.schema.Parse(open("cassandra_schema.avsc").read())
 
@@ -27,9 +43,7 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 directory = '../sensordata/EMB3_dataset_2017'
 files = os.listdir(directory)
 
-# TODO: change the schema for python kafka consumer so that it 
-# does transform that data at receiving end and can put it into the database
-period =  1 #1 day, is expressed in terms of lowest granularity ie. hours/days
+period =  24 #1 day, is expressed in terms of lowest granularity ie. hours/days
 obs = 5*period # number of observations i.e how many periods to take
 tstart = -1
 tstop = -1
@@ -40,19 +54,23 @@ for filename in files:
         continue
     with open(os.path.join(directory, filename), 'rb') as f:
         for line in f:                
+            msg = line.decode('utf-8').split(',')
+            to_send = msg_processing(msg)
             if agg_count == 0:
                 # this is a first timestamp for spark
-                spark_msg = line.decode('utf-8').split(',')
-                tstart = spark_msg[0] + " " + spark_msg[1]
-            future = producer.send('emb', emb_line_processing(line))
+                tstart = to_send['timestamp']
+            bytes_writer = io.BytesIO()
+            encoder = avro.io.BinaryEncoder(bytes_writer)
+            writer.write(to_send, encoder)
+            raw_bytes = bytes_writer.getvalue()
+            future = producer.send('emb', raw_bytes)
             future.get(timeout=10)
             time.sleep(0.5)
 
             agg_count += 1
             if (agg_count == obs): 
                 # this is a second timestamp for spark
-                spark_msg = line.decode('utf-8').split(',')
-                tstop = spark_msg[0] + " " + spark_msg[1]
+                tstop = to_send['timestamp'] 
                 bytes_writer = io.BytesIO()
                 encoder = avro.io.BinaryEncoder(bytes_writer)
                 spark_writer.write({"timestamp_start": tstart, 
